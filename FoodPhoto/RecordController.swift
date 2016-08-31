e0f0dab39bcc7f0ee8cf1dc4cae20f8a3661da1c
David Barley <ghuitster@gmail.com>
2016-08-30 21:29:33 -0600

    Finished the image capturing section, pretty sure

--- FoodPhoto/RecordController.swift
+++ FoodPhoto/RecordController.swift
@@ -1,16 +1,10 @@
-//
-//  StudentInformationController.swift
-//  FoodPhoto
-//
-//  Created by David Barley on 8/16/16.
-//  Copyright © 2016 Utah State University. All rights reserved.
-//
-
 import UIKit
 
-class RecordController: UIViewController, UITextFieldDelegate {
+class RecordController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet var studentIdField: UITextField!
     @IBOutlet var siteCodeField: UITextField!
+    let imagePicker: UIImagePickerController! = UIImagePickerController()
+    var imagePath: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,16 +25,74 @@ class RecordController: UIViewController, UITextFieldDelegate {
         return false
     }
     
-//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-//        let studentId = self.studentIdField.text
-//        let siteCode = self.siteCodeField.text
-//        
-//        let destinationViewController = segue.destinationViewController as! BeforeAfterController
-//        destinationViewController.studentId = studentId
-//        destinationViewController.siteCode = siteCode
-//        
-//        self.studentIdField.text = ""
-//    }
-
+    func getPathStart() -> String {
+        let currentDate = NSDate()
+        let myFormat = NSDateFormatter.dateFormatFromTemplate("ddMMMyyyy", options: 0, locale: NSLocale.currentLocale())
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = myFormat
+        let convertedDate = dateFormatter.stringFromDate(currentDate)
+        
+        return self.studentIdField.text! + "-" + self.siteCodeField.text! + "-" + convertedDate + "-"
+    }
+    
+    @IBAction func takeBeforePicture(sender: UIBarButtonItem) {
+        self.imagePath = getPathStart() + "before.jpg"
+        takePicture()
+    }
+    
+    @IBAction func takeAfterPicture(sender: UIBarButtonItem) {
+        self.imagePath = getPathStart() + "after.jpg"
+        takePicture()
+    }
+    
+    func cameraIsAvailable() -> Bool {
+        return UIImagePickerController.isSourceTypeAvailable(.Camera) && UIImagePickerController.availableCaptureModesForCameraDevice(.Rear) != nil
+    }
+    
+    func takePicture() {
+        if cameraIsAvailable() {
+            imagePicker.allowsEditing = false
+            imagePicker.sourceType = .Camera
+            imagePicker.cameraCaptureMode = .Photo
+            imagePicker.delegate = self
+            presentViewController(imagePicker, animated: true, completion: {})
+        }
+    }
+    
+    func getDocumentsURL() -> NSURL {
+        let documentsURL = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)[0]
+        
+        return documentsURL
+    }
+    
+    func fileInDocumentsDirectory(filename: String) -> String {
+        let fileURL = getDocumentsURL().URLByAppendingPathComponent(filename)
+        
+        return fileURL.path!
+    }
+    
+    func resetForNewImage() {
+        self.studentIdField.text = ""
+        self.imagePath = ""
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        let imageData = UIImageJPEGRepresentation(image, 0.6)
+        
+        do {
+            try imageData!.writeToFile(fileInDocumentsDirectory(self.imagePath), options: .AtomicWrite)
+        } catch {
+            print(error)
+        }
+        
+        picker.dismissViewControllerAnimated(true, completion: {
+            self.resetForNewImage()
+        })
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: {
+            self.resetForNewImage()
+        })
+    }
 }
-
